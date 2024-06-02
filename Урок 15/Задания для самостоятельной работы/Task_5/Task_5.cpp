98ba706e6c6c94466d963a170e1a1fdf4c31fba2
Andrew <globalshow6@gmail.com>
2024-06-03 00:45:25 +0300

    Убрал лишние include

--- Урок 15/Задания для самостоятельной работы/Task_5/Task_5.cpp
+++ Урок 15/Задания для самостоятельной работы/Task_5/Task_5.cpp
@@ -1,5 +1,4 @@
 ﻿#include <iostream>
-#include <cstdlib>
 #include <ctime>
 
 //Написать программу, которая удаляет из
--- Урок 15/Задания для самостоятельной работы/Task_6/Task_6.cpp
+++ Урок 15/Задания для самостоятельной работы/Task_6/Task_6.cpp
@@ -1,5 +1,4 @@
 ﻿#include <iostream>
-#include <cstdlib>
 #include <ctime>
 
 // Функция для создания и заполнения двумерного массива случайными числами
