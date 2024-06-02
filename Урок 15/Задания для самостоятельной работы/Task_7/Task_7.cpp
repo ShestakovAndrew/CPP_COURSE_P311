cb95c4ef9afed0f488df5eae53c81d14885cdab7
Andrew <globalshow6@gmail.com>
2024-06-03 00:45:50 +0300

    Добавил решение для 7 задания

--- Урок 15/Задания для самостоятельной работы/Task_7/Task_7.cpp
+++ Урок 15/Задания для самостоятельной работы/Task_7/Task_7.cpp
@@ -1,20 +1,97 @@
-﻿// Task_7.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <ctime>
 
-#include <iostream>
-
-int main()
+// Функция для создания и заполнения двумерного массива случайными числами
+int** CreateAndFillMatrix(size_t rows, size_t cols)
 {
-    std::cout << "Hello World!\n";
+	int** arr = new int* [rows];
+	for (size_t i = 0; i < rows; ++i)
+	{
+		arr[i] = new int[cols];
+		for (size_t j = 0; j < cols; ++j)
+		{
+			arr[i][j] = rand() % 10;
+		}
+	}
+	return arr;
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+// Функция умножения двух матриц
+int** MultiplyMatrices(int** A, int** B, size_t M, size_t N, size_t K)
+{
+	// Создаем матрицу C размером MxK для результата
+	int** C = new int* [M];
+	for (size_t i = 0; i < M; ++i)
+	{
+		C[i] = new int[K]();
+	}
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+	// Выполняем умножение матриц
+	for (size_t i = 0; i < M; ++i)
+	{
+		for (size_t j = 0; j < K; ++j)
+		{
+			for (size_t n = 0; n < N; ++n)
+			{
+				C[i][j] += A[i][n] * B[n][j];
+			}
+		}
+	}
+
+	return C;
+}
+
+// Функция для печати матрицы
+void PrintMatrix(int** matrix, size_t rows, size_t cols)
+{
+	for (size_t i = 0; i < rows; ++i)
+	{
+		for (size_t j = 0; j < cols; ++j)
+		{
+			std::cout << matrix[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
+}
+
+int main() 
+{
+	setlocale(LC_ALL, "rus");
+	srand((unsigned int)time(0));
+
+	size_t M = 2, N = 3, K = 4;
+
+	// Создание матрицы A размером MxN
+	int** A = CreateAndFillMatrix(M, N);
+	std::cout << "Матрица A:" << std::endl;
+	PrintMatrix(A, M, N);
+
+	// Создание матрицы B размером MxN
+	int** B = CreateAndFillMatrix(N, K);
+	std::cout << "Матрица B:" << std::endl;
+	PrintMatrix(B, N, K);
+
+	// Умножение матриц A и B
+	int** C = MultiplyMatrices(A, B, M, N, K);
+
+	// Вывод результата
+	std::cout << "Матрица C (результат умножения A и B):" << std::endl;
+	PrintMatrix(C, M, K);
+
+	// Освобождение памяти
+	for (int i = 0; i < M; ++i) 
+	{
+		delete[] A[i];
+		delete[] C[i];
+	}
+	delete[] A;
+	delete[] C;
+
+	for (int i = 0; i < N; ++i) 
+	{
+		delete[] B[i];
+	}
+	delete[] B;
+
+	return 0;
+}
