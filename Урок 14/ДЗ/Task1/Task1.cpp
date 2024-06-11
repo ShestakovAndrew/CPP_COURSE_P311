e7a36f24abd3090d084f38f295d6f04d6de245b7
Andrew <globalshow6@gmail.com>
2024-06-11 14:47:52 +0300

    Определил работу по ДЗ 14 урока

---
+++ Урок 14/ДЗ/Task1/Task1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+
+std::string RemoveChar(std::string str, size_t index)
+{
+	if (index >= 0 && index < str.length()) 
+	{
+		str.erase(index, 1); // Удаляем символ с заданным индексом
+	}
+	return str;
+}
+
+int main() 
+{
+	std::string myString = "Hello, world!";
+	size_t charIndex = 7;
+
+	std::cout << "Исходная строка: " << myString << std::endl;
+
+	myString = RemoveChar(myString, charIndex);
+
+	std::cout << "Строка после удаления: " << myString << std::endl;
+
+	return 0;
+}
